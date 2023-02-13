8de0b007e86dbe6c023097bcdbf738e5c4f7391d
Alex Grove <OrkleDorkle15@gmail.com>
2023-02-13 15:40:16 -0500

    Add funtion to choose rock, paper, or scissors for computer

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("Testing")
+// make a function that chooses, at random, the computer selection
+    // function returns choice
+// ask user what they choose: rock, paper, or scissors
+    // store in a variable
+// make function that plays one round, declares the winner with explanation
+// make function that plays five rounds
+
+function getComputerChoice () {
+    let choice = ["rock", "paper", "scissors"];
+    let randomChoiceNum = Math.floor(Math.random()*choice.length);
+    return choice[randomChoiceNum];
+}
+
+console.log(getComputerChoice())
