7f41f5ca5e7774013eae1a000d5bfc9d9a6594ad
Alex Grove <OrkleDorkle15@gmail.com>
2023-02-14 14:43:00 -0500

    Create game() function that plays five games

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-// make function that plays one round, declares the winner with explanation
-    // takes player and computer choices as parameters
-    // goes through 
-// make function that plays five rounds
-
-// make a function that chooses, at random, the computer selection
-    // function returns choice
-
-
-
+// make function that chooses the computer play randomly from an array
 function getComputerChoice () {
     let choice = ["rock", "paper", "scissors"];
     let randomChoiceNum = Math.floor(Math.random()*choice.length);
     return choice[randomChoiceNum];
 }
 
+// make function that prompts user for their choice of rock, paper, or scissors
 function getPlayerChoice () {
     return prompt("What do you wish to play? Rock, paper, or scissors: ").toLowerCase()
 }
 
+// make function that plays one round, declares the winner with explanation
+    // takes player and computer choices as parameters
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || 
     playerSelection === "scissors" && computerSelection === "paper") {
         console.log(`You win!\nPlayer: ${playerSelection}\n   beats\nComputer: ${computerSelection}`);
+        return "win";
     } else if (computerSelection === "rock" && playerSelection === "scissors" || computerSelection === "paper" && 
     playerSelection === "rock" || computerSelection === "scissors" && playerSelection === "paper") {
         console.log(`You lose!\nComputer: ${computerSelection}\n   beats\nPlayer: ${playerSelection}`);
+        return "lose";
     } else if (playerSelection === computerSelection) {
         console.log("It's a tie...");
+        return 0;
     } else {
         console.log("Please pick a valid option.");
     }
 }
 
-playRound(getPlayerChoice(), getComputerChoice())
+// make function that plays five rounds
+    // take playRound as a parameter
+    // loops through 5 times 
+    // declares winner at the end
+function game() {
+    for (let x = 0; x < 5; x++) {
+        playRound(getPlayerChoice(), getComputerChoice());
+    }
+}
+
+game()
