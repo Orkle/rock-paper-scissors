128800181195a6a88ede9dc30ef0babccffe5a90
Alex Grove <OrkleDorkle15@gmail.com>
2023-02-14 17:53:49 -0500

    Add function to loop through 5 games

--- script.js
+++ script.js
@@ -16,26 +16,42 @@ function playRound (playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || 
     playerSelection === "scissors" && computerSelection === "paper") {
         console.log(`You win!\nPlayer: ${playerSelection}\n   beats\nComputer: ${computerSelection}`);
-        return "win";
+        return 1;
     } else if (computerSelection === "rock" && playerSelection === "scissors" || computerSelection === "paper" && 
     playerSelection === "rock" || computerSelection === "scissors" && playerSelection === "paper") {
         console.log(`You lose!\nComputer: ${computerSelection}\n   beats\nPlayer: ${playerSelection}`);
-        return "lose";
+        return 0;
     } else if (playerSelection === computerSelection) {
         console.log("It's a tie...");
-        return 0;
+        return null;
     } else {
         console.log("Please pick a valid option.");
     }
 }
 
 // make function that plays five rounds
-    // take playRound as a parameter
     // loops through 5 times 
     // declares winner at the end
 function game() {
-    for (let x = 0; x < 5; x++) {
-        playRound(getPlayerChoice(), getComputerChoice());
+    let win = 0;
+    let lose = 0;
+    for (let i = 0; i < 5; i++) {
+        let winLose = playRound(getPlayerChoice(), getComputerChoice());
+        if (winLose === 1) {
+            win += 1;
+        } else if (winLose === 0) {
+            lose += 1;
+        } else {
+            win += 0;
+            lose += 0;
+        }
+    }
+    if (win > lose) {
+        console.log(`You win! Score: ${win} to ${lose}`);
+    } else if (win < lose) {
+        console.log(`You lose... Score: ${win} to ${lose}`);
+    } else {
+        console.log("Tie Game!")
     }
 }
 
